8d31acd2af466852ffb5c5a6ee78627f42a2ec79
izumi <kizumi916@gmail.com>
2020-03-01 16:49:20 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   before_action :move_to_index, except: [:create, :destroy]
   def create
     @like = current_user.likes.create(tweet_id: params[:tweet_id])
-    redirect_to "/"
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     @like.destroy
-    redirect_to "/"
+    redirect_back(fallback_location: root_path)
   end
 end
